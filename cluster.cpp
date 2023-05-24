5c77ffb2db04b808d3b9187192649a59998045d9
sckwokyboom <sckwokyboom@gmail.com>
2023-05-25 00:17:24 +0700

    Added Task class, fixed calculation of current number of task. Removed one thread to optimize memory used.

--- cluster.cpp
+++ cluster.cpp
@@ -15,16 +15,23 @@ constexpr int TASKS_IN_LIST = 470;
 constexpr int L = 1500;
 constexpr int ITERATION = 5;
 
-typedef struct Task {
+typedef class Task {
+public:
   long repeatNum;
+
+  void doTask(double *globalRes) const {
+    for (long i = 0; i < repeatNum; i++) {
+      *globalRes += sin((double) i);
+    }
+  }
 } Task;
 
 struct ProcessSharedData {
   int procRank = 0;
   int commSize = 0;
   int iterCounter = 0;
-  int curTaskNum = 0;
-  int taskListSize = 0;
+  int curTaskNumToExecute = 0;
+  int curTaskListSize = 0;
   double globalRes = 0;
   double summaryDisbalance = 0;
   std::vector<Task> taskList;
@@ -32,7 +39,7 @@ struct ProcessSharedData {
 };
 
 void generateTaskList(ProcessSharedData *psd) {
-  psd->taskListSize = TASKS_IN_LIST;
+  psd->curTaskListSize = TASKS_IN_LIST;
   for (int i = psd->procRank * TASKS_IN_LIST; i < (psd->procRank + 1) * TASKS_IN_LIST; i++) {
     long repeatNum = std::abs(TASKS_IN_LIST / 2 - i % TASKS_IN_LIST)
                      * std::abs(psd->procRank + 1 - (psd->iterCounter % psd->commSize)) * L;
@@ -51,16 +58,10 @@ int getTaskFrom(int from, ProcessSharedData *psd) {
   Task receiveTask;
   MPI_Recv(&receiveTask, 1, MPI_INT, from, ANSWER_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
   pthread_mutex_lock(&psd->mutex);
-  psd->taskList[psd->taskListSize] = receiveTask;
-  psd->taskListSize++;
+  psd->taskList[psd->curTaskListSize] = receiveTask;
+  psd->curTaskListSize++;
   pthread_mutex_unlock(&psd->mutex);
   return SUCCESS;
-}
-
-void doTask(Task task, ProcessSharedData *psd) {
-  for (long i = 0; i < task.repeatNum; i++) {
-    psd->globalRes += sin(i);
-  }
 }
 
 void *routineSenderThread(void *processSharedDataArg) {
@@ -71,14 +72,14 @@ void *routineSenderThread(void *processSharedDataArg) {
     MPI_Recv(&flag, 1, MPI_INT, MPI_ANY_SOURCE, REQUEST_TAG, MPI_COMM_WORLD, &status);
     if (flag == TURN_OFF) break;
     pthread_mutex_lock(&psd->mutex);
-    if (psd->curTaskNum >= psd->taskListSize - 1) {
+    if (psd->curTaskNumToExecute >= psd->curTaskListSize - 1) {
       pthread_mutex_unlock(&psd->mutex);
       flag = FAIL;
       MPI_Send(&flag, 1, MPI_INT, status.MPI_SOURCE, ANSWER_TAG, MPI_COMM_WORLD);
       continue;
     }
-    psd->taskListSize--;
-    Task sendTask = psd->taskList[psd->taskListSize];
+    psd->curTaskListSize--;
+    Task sendTask = psd->taskList[psd->curTaskListSize];
     pthread_mutex_unlock(&psd->mutex);
     flag = SUCCESS;
     MPI_Send(&flag, 1, MPI_INT, status.MPI_SOURCE, ANSWER_TAG, MPI_COMM_WORLD);
@@ -87,33 +88,30 @@ void *routineSenderThread(void *processSharedDataArg) {
   return nullptr;
 }
 
-void *routineExecutorThread(void *processSharedDataArg) {
+void executeRoutine(ProcessSharedData *psd) {
   MPI_Barrier(MPI_COMM_WORLD);
-  auto *psd = static_cast<ProcessSharedData *> (processSharedDataArg);
-  double start, end;
+  double startTime;
   psd->iterCounter = 0;
   while (psd->iterCounter < ITERATION) {
     int countOfFinishedTasks = 0;
     int hasTasks = 1;
     pthread_mutex_lock(&psd->mutex);
-    psd->curTaskNum = 0;
+    psd->curTaskNumToExecute = 0;
     generateTaskList(psd);
     pthread_mutex_unlock(&psd->mutex);
-    start = MPI_Wtime();
+    startTime = MPI_Wtime();
     while (hasTasks) {
       pthread_mutex_lock(&psd->mutex);
-      if (psd->curTaskNum < psd->taskListSize) {
-        Task task = psd->taskList[psd->curTaskNum];
+      if (psd->curTaskNumToExecute < psd->curTaskListSize) {
+        Task task = psd->taskList[psd->curTaskNumToExecute];
+        psd->curTaskNumToExecute++;
         pthread_mutex_unlock(&psd->mutex);
-        doTask(task, psd);
+        task.doTask(&psd->globalRes);
         countOfFinishedTasks++;
-        pthread_mutex_lock(&psd->mutex);
-        psd->curTaskNum++;
-        pthread_mutex_unlock(&psd->mutex);
         continue;
       }
-      psd->curTaskNum = 0;
-      psd->taskListSize = 0;
+      psd->curTaskNumToExecute = 0;
+      psd->curTaskListSize = 0;
       pthread_mutex_unlock(&psd->mutex);
       hasTasks = 0;
       for (int i = 0; i < psd->commSize; i++) {
@@ -123,8 +121,8 @@ void *routineExecutorThread(void *processSharedDataArg) {
         }
       }
     }
-    end = MPI_Wtime();
-    double timeTaken = end - start;
+    double endTime = MPI_Wtime();
+    double timeTaken = endTime - startTime;
     double minTime, maxTime;
     MPI_Reduce(&timeTaken, &minTime, 1, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD);
     MPI_Reduce(&timeTaken, &maxTime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
@@ -136,7 +134,6 @@ void *routineExecutorThread(void *processSharedDataArg) {
   }
   int flag = TURN_OFF;
   MPI_Send(&flag, 1, MPI_INT, psd->procRank, REQUEST_TAG, MPI_COMM_WORLD);
-  return nullptr;
 }
 
 int main(int argc, char **argv) {
@@ -152,50 +149,47 @@ int main(int argc, char **argv) {
   MPI_Comm_rank(MPI_COMM_WORLD, &psd->procRank);
   MPI_Comm_size(MPI_COMM_WORLD, &psd->commSize);
   psd->taskList = std::vector<Task>(psd->commSize * TASKS_IN_LIST);
-  pthread_t threads_id[2];
+  pthread_t senderThreadId;
   pthread_mutex_init(&psd->mutex, nullptr);
   pthread_attr_t attrs;
 
   if (pthread_attr_init(&attrs) != 0) {
+    std::cerr << "Unable to initialize attributes in process: " << psd->procRank << std::endl;
     delete psd;
     MPI_Finalize();
-    std::cerr << "Unable to initialize attributes in process: " << psd->procRank << std::endl;
     return 0;
   }
 
   if (pthread_attr_setdetachstate(&attrs, PTHREAD_CREATE_JOINABLE) != 0) {
-    delete psd;
-    MPI_Finalize();
     std::cerr << "Error in setting attributes in process: " << psd->procRank << std::endl;
-    return 0;
-  }
-
-  if (pthread_create(&threads_id[0], &attrs, routineSenderThread, psd) != 0 ||
-      pthread_create(&threads_id[1], &attrs, routineExecutorThread, psd) != 0) {
     delete psd;
     MPI_Finalize();
-    std::cerr << "Unable to create a thread in process: " << psd->procRank << std::endl;
     return 0;
   }
-  double start, end;
+
+  if (pthread_create(&senderThreadId, &attrs, routineSenderThread, psd) != 0) {
+    std::cerr << "Unable to create a thread in process: " << psd->procRank << std::endl;
+    delete psd;
+    MPI_Finalize();
+    return 0;
+  }
+  double startTime, endTime;
   if (psd->procRank == 0) {
-    start = MPI_Wtime();
+    startTime = MPI_Wtime();
+  }
+  executeRoutine(psd);
+  if (pthread_join(senderThreadId, nullptr) != 0) {
+    std::cerr << "Unable to join a thread in process: " << psd->procRank << std::endl;
+    delete psd;
+    MPI_Finalize();
+    return 0;
   }
 
 
-  for (auto thread: threads_id) {
-    if (pthread_join(thread, nullptr) != 0) {
-      delete psd;
-      MPI_Finalize();
-      std::cerr << "Unable to join a thread in process: " << psd->procRank << std::endl;
-      return 0;
-    }
-  }
-
   if (psd->procRank == 0) {
-    end = MPI_Wtime();
-    double timeTaken = end - start;
-    std::cout << "Summary disbalance: " << psd->summaryDisbalance / (ITERATION) * 100 << std::endl;
+    endTime = MPI_Wtime();
+    double timeTaken = endTime - startTime;
+    std::cout << "Summary disbalance: " << 100 * psd->summaryDisbalance / ITERATION << " %" << std::endl;
     std::cout << "Global time: " << timeTaken << std::endl;
   }
 
